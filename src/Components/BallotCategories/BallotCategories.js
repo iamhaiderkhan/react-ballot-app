6c5bed29d2dc3069f0d5501356397871db5d88e1
Haider Khan <haiderkhan.live@gmail.com>
2023-02-14 15:50:29 +0500

    balloting app

--- src/Components/BallotCategories/BallotCategories.js
+++ src/Components/BallotCategories/BallotCategories.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import map from "lodash/map";
 import "./styles.css";
 import BallotVoteCard from "../BallotVoteCard/BallotVoteCard";
